bd2ead252b4fa0e6e4c1792a4addf2009d78d5db
Tiarê Balbi <me@tiarebalbi.com>
2013-11-02 12:36:35 -0200

    projeto jit e javascript

---
+++ javascript/src/main/java/br/com/caelum/fj91/js/Programa.java
@@ -0,0 +1,25 @@
+package br.com.caelum.fj91.js;
+
+import javax.script.ScriptEngine;
+import javax.script.ScriptEngineManager;
+import javax.script.ScriptException;
+import sun.org.mozilla.javascript.internal.NativeArray;
+
+public class Programa {
+	public static void main(String[] args) throws ScriptException {
+		ScriptEngineManager factory = new ScriptEngineManager();
+        ScriptEngine engine = factory.getEngineByName("JavaScript");
+
+	//a implementação atual do NativeArray implementa a interface List
+	//https://github.com/mozilla/rhino/blob/master/src/org/mozilla/javascript/NativeArray.java
+        NativeArray array = (NativeArray)engine.eval("new Array(1.1, 9.0, 4.7, 5.33)");
+        
+        System.out.println("Tamanho do array: " + array.getLength());
+        
+        System.out.println("Iterando no array e exibindo os elementos:");
+        
+        for(int i = 0; i < array.getLength(); i++) {
+        	System.out.println(array.get(i, null));
+        }
+	}
+}
