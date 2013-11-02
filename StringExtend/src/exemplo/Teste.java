8dc17811d7cb93b8c611886eda09f7f3fedf579c
Tiarê Balbi <me@tiarebalbi.com>
2013-11-02 16:59:34 -0200

    Mais exemplos

---
+++ StringExtend/src/exemplo/Teste.java
@@ -0,0 +1,13 @@
+package exemplo;
+
+public class Teste {
+	
+	public static void main(String[] args) {
+		if("".isBlank("a")) {
+			System.out.println("Em branco");
+		}else {
+			System.out.println("Com dados");
+		}
+	}
+
+}
---
+++ StringExtend/src/java/lang/String.java
@@ -0,0 +1,13 @@
+package java.lang;
+
+public class String {
+	
+	public Boolean isBlank(String data) {
+		if(data.equals("")) {
+			return true;
+		}
+		
+		return false;
+	}
+
+}
