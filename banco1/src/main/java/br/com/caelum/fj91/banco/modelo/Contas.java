612d4bf54c5eb3f5514ffb9ddea01abc8965e851
Tiarê Balbi <me@tiarebalbi.com>
2013-11-09 14:40:41 -0200

    Update Exemplos

--- banco1/src/main/java/br/com/caelum/fj91/banco/modelo/Contas.java
+++ banco1/src/main/java/br/com/caelum/fj91/banco/modelo/Contas.java
@@ -3,16 +3,22 @@ package br.com.caelum.fj91.banco.modelo;
 import java.util.Calendar;
 import java.util.Collection;
 import java.util.HashSet;
+import java.util.Set;
 
 /**
  * Organiza as contas do banco por semestre de abertura
  */
-public class Contas extends HashSet<Conta> {
+public class Contas {
 
 	private int totalContasPrimeiroSemestre = 0;
 	private int totalContasSegundoSemestre = 0;
 	
-	@Override
+	private Set<Conta> contas = new HashSet<Conta>();
+	
+	public int size() {
+		return contas.size();
+	}
+
 	public boolean add(Conta conta) {
 		int mes = conta.getDataAbertura().get(Calendar.MONTH);
 		if (mes < 6) {
@@ -21,10 +27,9 @@ public class Contas extends HashSet<Conta> {
 			totalContasSegundoSemestre++;
 		}
 		
-		return super.add(conta);
+		return contas.add(conta);
 	}
 
-	@Override
 	public boolean addAll(Collection<? extends Conta> c) {
 		for (Conta conta : c) {
 			int mes = conta.getDataAbertura().get(Calendar.MONTH);
@@ -35,7 +40,7 @@ public class Contas extends HashSet<Conta> {
 			}
 		}
 		
-		return super.addAll(c);
+		return contas.addAll(c);
 	}
 	
 	public int getTotalContasPrimeiroSemestre() {
